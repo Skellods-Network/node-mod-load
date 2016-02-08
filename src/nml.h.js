2905ea40836cc2e7129df812c70ef78d4227bad6
minecrawler <marco@skellods.de>
2016-02-08 16:26:46 +0100

    remove binary search tree dependency

--- src/nml.h.js
+++ src/nml.h.js
@@ -1,32 +1,21 @@
 'use strict';
 
-var bst = require('binary-search-tree');
-
 var NML = require('./nml-class.h.js');
 
 
-var nst = null;
+var nst = {};
 
 // This simple factory will become a full-fledged object thingy later on
 module.exports = function ($namespace) {
 
-    if (nst == null) {
-
-        nst = new bst.BinarySearchTree({
-
-            unique: true,
-        });
-    }
-
-    var obj = nst.search($namespace);
-    if (obj.length <= 0) {
+    if (!nst[$namespace]) {
 
         var newNS = new NML($namespace);
-        nst.insert($namespace, newNS);
+        nst[$namespace] = newNS;
         return newNS;
     }
     else {
 
-        return obj[0];
+        return nst[$namespace];
     }
 };
